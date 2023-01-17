9f65f187eb22571d01be56fcff197595c57e027e
keklick1337 <86933883+keklick1337@users.noreply.github.com>
2023-01-17 15:10:11 +0400

    change version number, and fix description on pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@
 
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='async7zip',
-    version='1.0',
+    version='1.0.0',
     author='Vladislav Tislenko',
     author_email='keklick1337@gmail.com',
     packages=find_packages(),
     install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
